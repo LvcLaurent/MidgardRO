9513bdf50254df9c714daaca17f5b7cfe016607e
LvcLaurent <lau.sion@gmail.com>
2026-07-15 14:22:13 +0200

    Revert "Grant faction titles through the real achievement system"
    This reverts commit 4eaa9cbe4b84a1152d8a029191bb0b5319c121c7.

--- server/src/map/achievement.hpp
+++ server/src/map/achievement.hpp
@@ -63,10 +63,10 @@ enum e_achievement_info {
 	ACHIEVEINFO_MAX,
 };
 
-enum e_title_table : uint32 {
+enum e_title_table : uint16 {
 	TITLE_NONE = 0,
 	TITLE_BASE = 1000,
-	TITLE_MAX = 90001, // Reign of Midgard: raised from 1046 to fit FACTION_TITLE_* (see common/mmo.hpp)
+	TITLE_MAX = 1046,
 };
 
 struct achievement_target {
